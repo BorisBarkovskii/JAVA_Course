1c16fef6724e1566e05b73bc659031be3ad5c227
unknown <boris.barkovski@gmail.com>
2023-04-15 21:44:47 +0300

    Task_3_1

--- src/main/java/Lesson_1/Task_2.java
+++ src/main/java/Lesson_1/Task_2.java
@@ -4,7 +4,7 @@ package homework1;
         Если в массиве есть числа, равные заданному, нужно перенести эти элементы в конец массива.
         Таким образом, первые несколько (или все) элементов массива должны быть отличны от заданного,
         а остальные - равны ему. */
-public class MainClassTask2 {
+public class пшMainClassTask2 {
     public static void main(String[] args) {
         int[] nums = {3, 1, 2, 3, 1, 3};
         for (int x : nums) System.out.print(x + " ");
